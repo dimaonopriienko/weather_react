6e26163ebf614826a4d820a427719d5f68af68fa
Dima Onopriienko <Dima Onopriienko>
2018-04-01 14:50:52 +0300

    weather app

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import WeatherDisplay from './WeatherDisplay';
+//import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+//import "bootswatch/dist/journal/bootstrap.css";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
+
+const PLACES = [
+  { name: "Palo Alto", zip: "94303" },
+  { name: "San Jose", zip: "94088" },
+  { name: "Santa Cruz", zip: "95062" },
+  { name: "Honolulu", zip: "96803" }
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePlace: 0,
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              React Simple Weather App
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+        <Grid>
+          <Row>
+            <Col md={4} sm={4}>
+              <h3>Select a city</h3>
+              <Nav
+                bsStyle="pills"
+                stacked
+                activeKey={this.state.activePlace}
+                onSelect={index => {
+            this.setState({ activePlace: index });
+          }}
+              >
+                {PLACES.map((place, index) => (
+                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                ))}
+              </Nav>
+            </Col>
+            <Col md={8} sm={8}>
+              <WeatherDisplay key={this.state.activePlace} zip={PLACES[this.state.activePlace].zip} />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default App;
